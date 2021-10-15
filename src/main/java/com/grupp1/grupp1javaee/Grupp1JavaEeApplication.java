2cddd5b40303bfb912b3ec1408559ce45f6fe801
Tengill91 <robban_t@hotmail.com>
2021-10-15 09:58:54 +0200

    Första committen / Robban

---
+++ src/main/java/com/grupp1/grupp1javaee/Grupp1JavaEeApplication.java
@@ -0,0 +1,13 @@
+package com.grupp1.grupp1javaee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupp1JavaEeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupp1JavaEeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupp1/grupp1javaee/Grupp1JavaEeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp1.grupp1javaee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupp1JavaEeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
