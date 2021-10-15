52e63604f23471e2b120452b193b8f4eb3353bc0
Tengill91 <robban_t@hotmail.com>
2021-10-15 10:36:40 +0200

    test / Robban

--- src/main/java/com/grupp1/grupp1javaee/Grupp1JavaEeApplication.java
+++ src/main/java/com/grupp1/grupp1javaee/Grupp1JavaEeApplication.java
@@ -8,6 +8,11 @@ public class Grupp1JavaEeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Grupp1JavaEeApplication.class, args);
+
+        System.out.println("hej/Robban");
+
     }
+
+
 
 }
