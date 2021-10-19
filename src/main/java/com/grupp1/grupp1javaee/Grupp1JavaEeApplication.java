f87fe2e5e28ad9c923a105c6edbf3f17081b99b8
Tengill91 <robban_t@hotmail.com>
2021-10-19 14:57:50 +0200

    projekt mall /Parprogrammering

--- src/main/java/com/grupp1/grupp1javaee/Grupp1JavaEeApplication.java
+++ src/main/java/com/grupp1/grupp1javaee/Grupp1JavaEeApplication.java
@@ -1,17 +1,22 @@
 package com.grupp1.grupp1javaee;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class Grupp1JavaEeApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Grupp1JavaEeApplication.class, args);
 
-        System.out.println("hej/Robban");
+
 
     }
+
 
 
 
